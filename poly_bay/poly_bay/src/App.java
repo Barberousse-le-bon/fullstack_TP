65092474d4bd81a8c8fad494546b6dd582830fa1
HYOT Lucas <lucash21200303@gmail.com>
2024-05-21 09:10:28 +0200

    la classe PolyBayDatabase fonctionne correctement

---
+++ poly_bay/poly_bay/src/App.java
@@ -0,0 +1,14 @@
+import database.PolyBayDatabase;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+
+        PolyBayDatabase poly_bay = new PolyBayDatabase();
+
+
+
+    }
+}
---
+++ poly_bay/poly_bay/src/database/PolyBayDatabase.java
@@ -0,0 +1,16 @@
+package database;
+
+import java.sql.SQLException;
+
+public class PolyBayDatabase extends MySQLDatabase{
+    
+
+    public PolyBayDatabase() throws SQLException{
+
+    
+        super("localhost",3306,"poly_bay","poly_bay","poly_bay");
+
+    }
+
+
+}
